3eced197fd70f4d3ea765c9d17700f088dad4776
wjjsn <wjjsn@qq.com>
2026-05-20 17:08:10 +0800

    feat:新增`目标板状态`

--- src/find_line_lib/status_switcher.py
+++ src/find_line_lib/status_switcher.py
@@ -14,7 +14,14 @@ class 模型识别状态(Enum):
     直行 = auto()
     右 = auto()
 
+class 目标板状态(Enum):
+    未发现 = auto()
+    已发现 = auto()
+    通过中 = auto()
+
+
 class status_switcher():
     def __init__(self):
         self.圆坏:圆坏状态=圆坏状态.未发现
         self.模型识别:模型识别状态=模型识别状态.未发现
+        self.目标板:目标板状态=目标板状态.未发现
