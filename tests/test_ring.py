f89d558e74936cba494c3e88b5babab2c53c6b22
wjjsn <wjjsn@qq.com>
2026-05-21 21:36:21 +0800

    test:新增测试`准备入环`

--- tests/test_ring.py
+++ tests/test_ring.py
@@ -1,8 +1,8 @@
-from find_line_lib.ring import 发现圆坏
+from find_line_lib.ring import 发现圆坏,准备入环
 import cv2
 
 if __name__ == '__main__':
-    rgb_img = cv2.imread('png/圆环 (2).jpg')
+    rgb_img = cv2.imread('png/found_roundabout.jpg')
     assert rgb_img is not None
 
     gray = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2GRAY)
@@ -17,5 +17,28 @@ if __name__ == '__main__':
     cv2.circle(rgb_img, rp, 3, (255, 0, 0), -1)
     cv2.imshow('rgb_img', rgb_img)
     cv2.waitKey(0)
+
     cv2.destroyAllWindows()
-    print(result)
+    ######################################################################
+    # 测试**准备入环**
+    # 
+    for img in ["png/enter_roundabout_0.jpg","png/enter_roundabout_1.jpg"]:
+        rgb_img = cv2.imread(img)
+        assert rgb_img is not None
+    
+        gray = cv2.cvtColor(rgb_img, cv2.COLOR_BGR2GRAY)
+        _, bin_img = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+    
+        result = 准备入环(bin_img)
+        assert result is not None
+    
+        lp, rp = result
+    
+        cv2.circle(rgb_img, lp, 3, (0, 0, 255), -1)
+        cv2.circle(rgb_img, rp, 3, (255, 0, 0), -1)
+        cv2.imshow('bin_img', bin_img)
+        cv2.imshow('rgb_img', rgb_img)
+        cv2.waitKey(0)
+        print(result)
+    cv2.destroyAllWindows()
+    
