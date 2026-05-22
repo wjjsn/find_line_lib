3c3a8978fdc9b20b068e7ec6867be053ff68e562
wjjsn <wjjsn@qq.com>
2026-05-22 15:46:07 +0800

    feat!:新增`圆坏.类型`枚举类，将`圆坏状态`改为`圆坏.状态`

--- src/find_line_lib/status_switcher.py
+++ src/find_line_lib/status_switcher.py
@@ -1,12 +1,17 @@
 from enum import Enum,auto
 
-class 圆坏状态(Enum):
-    已发现 = auto()
-    准备入环 = auto()
-    # 已入环 = auto()
-    准备出环 = auto()
-    出环中 = auto()
-    未发现 = auto()
+class 圆坏(Enum):
+    class 状态(Enum):
+        已发现 = auto()
+        准备入环 = auto()
+        # 已入环 = auto()
+        准备出环 = auto()
+        出环中 = auto()
+        未发现 = auto()
+    class 类型(Enum):
+        无 = auto()
+        左 = auto()
+        右 = auto()
 
 class 模型识别状态(Enum):
     未发现 = auto()
@@ -22,6 +27,7 @@ class 目标板状态(Enum):
 
 class status_switcher():
     def __init__(self):
-        self.圆坏:圆坏状态=圆坏状态.未发现
+        self.圆坏状态:圆坏.状态=圆坏.状态.未发现
+        self.圆坏类型:圆坏.类型=圆坏.类型.无
         self.模型识别:模型识别状态=模型识别状态.未发现
         self.目标板:目标板状态=目标板状态.未发现
